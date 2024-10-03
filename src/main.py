984bcf894b67e4229f31ee13fb76591a8e16caf6
p-lague <p.lague@sudogroup.fr>
2024-10-03 16:05:44 +0200

    PL - TPOT notebooks all functionnal and documented

--- src/main.py
+++
@@ -1,6 +0,0 @@
-""" this module provide a function to print hello """
-
-import package1.module1
-
-print("main python file")
-package1.module1.print_hello()
--- src/package1/module1.py
+++
@@ -1,6 +0,0 @@
-""" this module provide a function to print hello """
-
-
-def print_hello():
-    """this function only print hello"""
-    print("Hello from module1")
